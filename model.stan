0d434dd12378586eb2d56b224b19da0d80269751
bgautijonsson <bgautijonsson@gmail.com>
2025-01-22 21:29:54 +0000

    Adding some preliminary modeling using offensive and defensive capability as well

--- model.stan
+++ model.stan
@@ -6,19 +6,44 @@ data {
   int<lower=0> N; // games
   array[N] int<lower=1, upper=K> team1; // team 1 for game n
   array[N] int<lower=1, upper=K> team2; // team 2 for game n
-  array[N] int<lower=0, upper=1> y; // winner for game n
+  array[N] int<lower=0> goals_team1; // team 1 for game n
+  array[N] int<lower=0> goals_team2; // team 2 for game n
+  array[N] int<lower=0, upper=1> winner; // winner for game n
   int<lower=1, upper=K> pred_team1;
   int<lower=1, upper=K> pred_team2;
 }
 
 parameters {
-  sum_to_zero_vector[K] alpha; // ability for teams
+  sum_to_zero_vector[K] z_alpha; // ability for teams
+  vector[K] offense;
+  vector[K] defense;
+  real<lower = 0> mean_goals;
+  real<lower = 0> sd_goals;
+  vector[N] z_game_goals;
+
+  real<lower = 0> beta_offense;
+  real<lower = 0> beta_defense;
+}
+
+transformed parameters {
+  vector[N] game_goals = mean_goals + sd_goals * z_game_goals;
+  vector[K] alpha = z_alpha + beta_offense * offense - beta_defense * defense;
 }
 
 
 model {
   alpha ~ std_normal();
-  y ~ bernoulli_logit(alpha[team2] - alpha[team1]);
+  offense ~ std_normal();
+  defense ~ std_normal();
+  mean_goals ~ normal(0, 2);
+  sd_goals ~ normal(0, 2);
+  z_game_goals ~ std_normal();
+  beta_offense ~ std_normal();
+  beta_defense ~ std_normal();
+  z_alpha ~ std_normal();
+  goals_team1 ~ poisson_log(game_goals + offense[team1] - defense[team2]);
+  goals_team2 ~ poisson_log(game_goals + offense[team2] - defense[team1]);
+  winner ~ bernoulli_logit(alpha[team2] - alpha[team1]);
 }
 
 generated quantities {
--- modeling.R
+++ modeling.R
@@ -6,10 +6,12 @@ library(metill)
 theme_set(theme_metill())
 
 
-d <- read_sheet("https://docs.google.com/spreadsheets/d/1ctStWrpf3JSe--qObOrej1qPlHLPYeTbw9FoixESMZ0/edit?gid=0#gid=0") |> 
+d <- read_sheet(
+  "https://docs.google.com/spreadsheets/d/1ctStWrpf3JSe--qObOrej1qPlHLPYeTbw9FoixESMZ0/edit?gid=0#gid=0"
+) |>
   janitor::clean_names()
 
-d |> 
+d |>
   write_csv("data.csv")
 
 
@@ -17,44 +19,48 @@ teams <- unique(c(d$team1, d$team2))
 
 teams <- tibble(
   team = teams
-) |> 
-  arrange(team) |> 
+) |>
+  arrange(team) |>
   mutate(
     team_nr = row_number()
   )
 
 
 
-model_d <- d |> 
-  filter(winner != 0) |> 
+model_d <- d |>
+  filter(winner != 0) |>
   bind_rows(
-    d |> 
+    d |>
       filter(
         winner == 0
-      ) |> 
+      ) |>
       crossing(
         wnr = c(1, 2)
-      ) |> 
-      select(-winner) |> 
+      ) |>
+      select(-winner) |>
       rename(winner = wnr)
-  ) |> 
+  ) |>
   inner_join(
-    teams |> 
+    teams |>
       rename(
         team_nr1 = team_nr
       ),
     by = join_by(team1 == team)
-  ) |> 
+  ) |>
   inner_join(
-    teams |> 
+    teams |>
       rename(
         team_nr2 = team_nr
       ),
     by = join_by(team2 == team)
-  ) |> 
+  ) |>
   select(
-    team_nr1, team_nr2, winner
-  ) |> 
+    team_nr1,
+    team_nr2,
+    winner,
+    goals_team1 = goals1,
+    goals_team2 = goals2
+  ) |>
   mutate(
     winner = winner - 1
   )
@@ -64,14 +70,16 @@ K <- length(unique(teams$team))
 N <- nrow(model_d)
 team1 <- model_d$team_nr1
 team2 <- model_d$team_nr2
-y <- model_d$winner
+winner <- model_d$winner
+goals_team1 <- model_d$goals_team1
+goals_team2 <- model_d$goals_team2
 
-pred_team2 <- teams |> 
-  filter(team == "Iceland") |> 
+pred_team2 <- teams |>
+  filter(team == "Iceland") |>
   pull(team_nr)
 
-pred_team1 <- teams |> 
-  filter(team == "Egypt") |> 
+pred_team1 <- teams |>
+  filter(team == "Egypt") |>
   pull(team_nr)
 
 stan_data <- list(
@@ -79,7 +87,9 @@ stan_data <- list(
   N = N,
   team1 = team1,
   team2 = team2,
-  y = y,
+  goals_team1 = goals_team1,
+  goals_team2 = goals_team2,
+  winner = winner,
   pred_team2 = pred_team2,
   pred_team1 = pred_team1
 )
@@ -96,17 +106,17 @@ results$summary("pred_outcome")
 
 alpha <- results$summary("alpha")
 
-alpha |> 
+alpha |>
   mutate(
     team_nr = parse_number(variable)
-  ) |> 
+  ) |>
   inner_join(
     teams,
     by = join_by(team_nr)
-  ) |> 
+  ) |>
   mutate(
     team = fct_reorder(team, median)
-  ) |> 
+  ) |>
   ggplot(aes(median, team)) +
   geom_point(
     size = 3
@@ -129,31 +139,31 @@ alpha |>
     title = "Mat á 'getu' landsliða eftir frammistöðu á HM hingað til"
   )
 
-results$draws("ranked") |> 
-  as_draws_df() |> 
-  as_tibble() |> 
-  pivot_longer(c(-.chain, -.draw, -.iteration)) |> 
+results$draws("ranked") |>
+  as_draws_df() |>
+  as_tibble() |>
+  pivot_longer(c(-.chain, -.draw, -.iteration)) |>
   mutate(
     placement = parse_number(name)
-  ) |> 
+  ) |>
   filter(
     placement == 1
-  ) |> 
-  count(team_nr = value) |> 
+  ) |>
+  count(team_nr = value) |>
   mutate(
     p = n / sum(n)
-  ) |> 
+  ) |>
   inner_join(
     teams
-  ) |> 
+  ) |>
   mutate(
     team = fct_reorder(team, p)
-  ) |> 
+  ) |>
   ggplot(aes(p, team)) +
   geom_segment(
     aes(xend = 0, yend = team),
     linewidth = 0.1
-    ) +
+  ) +
   geom_point(
     size = 2
   ) +
@@ -173,3 +183,64 @@ results$draws("ranked") |>
     subtitle = "Ekki líkur á að vinna HM, því það fer líka eftir því hvaða liðum þú mætir osf"
   )
 
+
+offense <- results$summary("offense") |>
+  mutate(
+    type = "offense"
+  ) |>
+  mutate(
+    team_nr = parse_number(variable)
+  ) |>
+  inner_join(
+    teams,
+    by = join_by(team_nr)
+  ) |>
+  mutate(
+    team = fct_reorder(team, median)
+  )
+
+defense <- results$summary("defense") |>
+  mutate(
+    type = "defense"
+  ) |>
+  mutate(
+    team_nr = parse_number(variable)
+  ) |>
+  inner_join(
+    teams,
+    by = join_by(team_nr)
+  ) |>
+  mutate(
+    team = fct_reorder(team, median)
+  )
+
+offense |>
+  ggplot(aes(median, team)) +
+  geom_pointrange(
+    aes(xmin = q5, xmax = q95, y = team, color = "Offense"),
+    linewidth = 0.4,
+    size = 1
+  ) +
+  geom_pointrange(
+    data = defense,
+    aes(xmin = q5, xmax = q95, y = team, color = "Defense"),
+    linewidth = 0.4,
+    size = 1
+  ) +
+  scale_x_continuous(
+    guide = ggh4x::guide_axis_truncated(),
+    expand = expansion(mult = c(0.01, 0.05))
+  ) +
+  scale_y_discrete(
+    guide = ggh4x::guide_axis_truncated()
+  ) +
+  labs(
+    x = NULL,
+    y = NULL,
+    title = "Mat á sókn og vörn landsliða eftir frammistöðu á HM hingað til"
+  )
+
+results$summary("game_goals")
+results$summary(c("mean_goals", "sd_goals"))
+
+results$summary(c("beta_offense", "beta_defense"))
